b5c95ae60d093d6cbbb2dab5bdd1b29511b4039f
Yerdaulet Batalov <erdauletbatalov@gmail.com>
2025-02-03 17:30:23 +0500

    add leetcode-605
    Signed-off-by: batalov.erdaulet <batalov.erdaulet@wb.ru>

---
+++ 605.go
@@ -0,0 +1,43 @@
+package main
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	for i := range flowerbed {
+		if canPlaceFlower(flowerbed, i) {
+			n--
+			if n == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func canPlaceFlower(flowerbed []int, index int) bool {
+	if len(flowerbed) == 1 {
+		if flowerbed[index] == 0 {
+			return true
+		} else {
+			return false
+		}
+	}
+	switch index {
+	case 0:
+		if flowerbed[index] == 0 && flowerbed[index+1] == 0 {
+			return true
+		} else {
+			return false
+		}
+	case len(flowerbed) - 1:
+		if flowerbed[index] == 0 && flowerbed[index-1] == 0 {
+			return true
+		} else {
+			return false
+		}
+	default:
+		if flowerbed[index] == 0 && flowerbed[index-1] == 0 && flowerbed[index+1] == 0 {
+			return true
+		} else {
+			return false
+		}
+	}
+}
