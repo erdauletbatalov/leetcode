c4c3cd92983cd1f2a5710d2e9c1015d42372d607
Yerdaulet Batalov <erdauletbatalov@gmail.com>
2025-02-21 15:50:28 +0500

    rebase to main
    Signed-off-by: Yerdaulet Batalov <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
-	mystack := Constructor225()
-	mystack.Push(5)
-	mystack.Push(4)
-	mystack.Push(3)
-	mystack.Push(2)
-	mystack.Pop()
-	mystack.Pop()
-	mystack.Pop()
-	mystack.Push(2)
-	mystack.Push(2)
-	mystack.Empty()
+	fmt.Println(fib(10))
 }
